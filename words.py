aeb6afbd3ca773655ff905166ac758226ff577c8
Aaron24115 <ironmaximus007@gmail.com>
2025-10-01 14:08:34 +0200

    added new words

--- words.py
+++ words.py
@@ -2,6 +2,8 @@ word_list = [
     "cat", "dog", "sun", "hat", "car",
     "book", "tree", "fish", "ball", "cup",
     "star", "moon", "shoe", "bird", "pen",
-    "nigger"
+    "surprise", "crab", "sunshine", "umbrella",
+    "red", "deer", "energy", "dinosaur", "chest",
+    
 
 ]
